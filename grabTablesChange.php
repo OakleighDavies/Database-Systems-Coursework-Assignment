5db056efc627989d05c6db9ee447c45bb0baec1f
Oakleigh Davies <158189448+OakleighDavies@users.noreply.github.com>
2026-02-12 11:15:21 +0000

    Update grabTablesChange.php licensing

--- grabTablesChange.php
+++ grabTablesChange.php
@@ -1,8 +1,20 @@
-# Copyright (c) 2025 Oakleigh Davies. All rights reserved.
+<!--
+Copyright (C) 2025 Oakleigh Davies.
 
-# This work is licensed under a Creative Commons Attribution-NonCommercial 4.0 International License (CC BY-NC 4.0).
-# You should have received a copy of the license along with this work. 
-# If not, see <https://creativecommons.org/licenses/by-nc/4.0/>.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org>.
+-->
+
 
 <!DOCTYPE html>
 <html>
